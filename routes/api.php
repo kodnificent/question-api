45d595612c5bfe69a4a331469a554db73fa0ad4a
kodnificent <mbamaravictor@gmail.com>
2020-11-18 04:25:30 +0100

    dockerize app

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ChoiceController;
-use App\Http\Controllers\Api\QuestionController;
+use App\Http\Controllers\API\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
